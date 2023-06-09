a3729028c26ddb26b6e44fe775440a0a2915140f
Pranita Raut <95748550+Pranita3434@users.noreply.github.com>
2023-06-09 13:04:08 +0530

    Add files via upload

---
+++ JAVA U/addmul.java
@@ -0,0 +1,23 @@
+class addmul 
+{
+  public static void main(String args[])
+ {
+    int no = 123;
+    int sum = 0;
+    int mul = 1;
+
+    while (no > 0) 
+   {
+      int digit = no % 10;
+      sum = digit + sum;
+      mul = digit * mul;
+      no = no / 10;
+
+   }
+   if(sum==mul)
+      System.out.println("Special");
+   else
+      System.out.println("Not Special");
+
+  }
+}
